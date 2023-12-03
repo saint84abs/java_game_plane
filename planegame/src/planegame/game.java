c056d56e8b658d842f65732a69940c3a1760d8e8
CKIRUser <CKIRUser@953857>
2023-12-03 23:40:10 +0900

    image setup and paint

--- planegame/src/planegame/game.java
+++ planegame/src/planegame/game.java
@@ -16,7 +16,7 @@ public class game extends JFrame implements ActionListener {
 	private JButton bt_1 = new JButton();
 	private JButton bt_2 = new JButton();
 	
-	private int x = 0, y = 0;
+	private int x = 220, y = 220;
 	
 	private ImageIcon background = new ImageIcon("../planegame/image/myBackGound.jpg");
 	private ImageIcon myPlane = new ImageIcon("../planegame/image/myPlane.png");
@@ -32,20 +32,50 @@ public class game extends JFrame implements ActionListener {
 		
 		backgroundLabel.setIcon(background);
 		myPlaneLabel.setIcon(myPlane);
+		//myPlaneLabel.setBackground(null);
 		
-		JPanel plane = new JPanel();
-		plane.add(myPlaneLabel);
+		JPanel plane = new JPanel() {
+			public void paintComponent(Graphics g) {
+				g.drawImage(myPlane.getImage(), 0, 0, myPlane.getIconWidth(), myPlane.getIconHeight(), null);
 
-		plane.setBounds(100, 100, myPlaneLabel.getWidth(), myPlaneLabel.getHeight());
+			}
+		};
+		plane.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				if (e.getKeyChar() == 'w' ) {
+					y++;
+				}
+				if (e.getKeyChar() == 's') {
+					y--;
+				}
+				
+			}
+		});
+		plane.setBounds(x, y, myPlane.getIconWidth(), myPlane.getIconHeight());
 		backgroundLabel.setBounds(0, 0, background.getIconWidth(), background.getIconHeight());
 		
-		add("background", backgroundLabel);
 		add("myPlane", plane);
+		add("background", backgroundLabel);
 		
-		//pack();
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 	}
+	
 	
 	
 	@Override
@@ -55,7 +85,16 @@ public class game extends JFrame implements ActionListener {
 	
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyChar() == 'w') {
-			
+			y++;
+		}
+		if (e.getKeyChar() == 's') {
+			y--;
+		}
+		if (e.getKeyChar() == 'd') {
+			x++;
+		}
+		if (e.getKeyChar() == 'a') {
+			x--;
 		}
 	}
 	
