853cb7095ce53aec1e0c82b61c31ca703cfb6ce8
CKIRUser <CKIRUser@953857>
2023-12-12 23:36:07 +0900

    move 성공, 더블 버퍼링 필요

--- planegame/src/planegame/game.java
+++ planegame/src/planegame/game.java
@@ -13,7 +13,6 @@ import com.sun.tools.javac.Main;
 public class game extends JFrame {
 	private JButton bt_1 = new JButton();
 	private JButton bt_2 = new JButton();
-	private int x = 200, y = 720;
 	
 	private ImageIcon background = new ImageIcon("../planegame/image/myBackGound.jpg");
 	
@@ -24,6 +23,9 @@ public class game extends JFrame {
 	
 	private Thread thread_plane = new Thread(new MyRunnable());
 	
+	private int x = 200, y = 720;
+	private boolean isUP, isDOWN, isLEFT, isRIGHT;
+	
 	public game(String title) {
 		super(title);
 		setBounds(300, 150, background.getIconWidth(), background.getIconHeight());
@@ -31,6 +33,7 @@ public class game extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 		addKeyListener(new MykeyListner());
+		thread_plane.start();
 		// 스레드 시작시 keylistner에서 thread로 true, false 로 값 전달
 		// 실질적인 이동은 thread에서
 		// https://dreamchallenger.blogspot.com/2011/08/keylistener-thread.html
@@ -45,6 +48,42 @@ public class game extends JFrame {
 	
 	public static void main(String args[]) {
 		game g = new game("1944");
+		
+	}
+	
+	public void Move() {
+		//https://binghedev.tistory.com/56
+		// 비행기가 계속 깜빡거리는데, 더블 버퍼링을 통해 해결 가능?
+		while(isUP == true || isDOWN == true || isLEFT == true || isRIGHT == true) {
+			if (isUP == true) {
+				if (y > 450)
+					y -= 1;
+				else 
+					y = 450;
+				repaint();
+			}
+			if (isDOWN == true) {
+				y += 1;
+				repaint();
+			}
+			if (isLEFT == true) {
+				if (x < 0) 
+					x = 0;
+				else 
+					x -= 1;
+				repaint();
+			}
+			if (isRIGHT == true) {
+				x += 1;
+				repaint();
+			}
+			try {
+				Thread.sleep(8);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	class MyRunnable implements Runnable {
@@ -52,13 +91,8 @@ public class game extends JFrame {
 		
 		@Override
 		public void run() {
-			try {
-				while(isRunning) {
-					repaint();
-					Thread.sleep(1);									
-				}
-			} catch (Exception e) {				
-				System.out.println(Thread.currentThread().getName());
+			while(isRunning) {
+				Move();									
 			}
 		}
 	}
@@ -68,41 +102,40 @@ public class game extends JFrame {
 		public void keyTyped(KeyEvent e) {}
 		@Override
 		public void keyPressed(KeyEvent e) {
-			int keyCode = e.getKeyCode();
-			
-			switch (keyCode) {
-			case KeyEvent.VK_A:
-				System.out.println("A key is pressed!");
-				if (x < 0)
-					x = 0;
-				x -= 3;
-				repaint();
+			// TODO Auto-generated method stub
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				isLEFT = true;
 				break;
-			case KeyEvent.VK_D:
-				System.out.println("D key is pressed!");
-				if (x > background.getIconWidth() - planeImage.getWidth(null)) 
-					x = background.getIconWidth() - planeImage.getWidth(null);
-				x += 3;
-				repaint();
+			case KeyEvent.VK_RIGHT:
+				isRIGHT = true;
 				break;
-			case KeyEvent.VK_W:
-				System.out.println("W key is pressed!");
-				if (y < 550)
-					y = 550;
-				y -= 3;
-				repaint();
+			case KeyEvent.VK_UP:
+				isUP = true;
 				break;
-			case KeyEvent.VK_S:
-				System.out.println("S key is pressed!");
-				if (y > background.getIconHeight() - planeImage.getHeight(null))
-					y = background.getIconHeight() - planeImage.getHeight(null);
-				y += 3;
-				repaint();
+			case KeyEvent.VK_DOWN:
+				isDOWN = true;
 				break;
 			}
-			
 		}
+
 		@Override
-		public void keyReleased(KeyEvent e) {}
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				isLEFT = false;
+				break;
+			case KeyEvent.VK_RIGHT:
+				isRIGHT = false;
+				break;
+			case KeyEvent.VK_UP:
+				isUP = false;
+				break;
+			case KeyEvent.VK_DOWN:
+				isDOWN = false;
+				break;
+			}
+		}
 	}
 }
